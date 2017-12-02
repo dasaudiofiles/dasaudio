e027aba26574cff0b73fe25c28ab29d75f5e0806
Arthur Henry <arthurhenry@Arthurs-MacBook-Pro.local>
2017-12-02 15:54:17 -0500

    new src folder

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Compenent {
+    render() {
+        return (
+            <div className="App">
+                Hello World!
+            </div>
+        )
+    }
+}
+
+export default App;
